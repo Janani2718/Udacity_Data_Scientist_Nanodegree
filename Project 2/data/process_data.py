c750843bd917c19dee01391639e19dfa0327c79c
Janani2718 <81457427+Janani2718@users.noreply.github.com>
2022-01-21 18:05:00 +0530

    Update process_data.py

--- Project 2/data/process_data.py
+++ Project 2/data/process_data.py
@@ -5,6 +5,17 @@ import numpy as np
 
 
 def load_data(messages_filepath, categories_filepath):
+    '''
+    This function loads the dataset from given filepath
+    
+    Input:
+    messages_filepath - Filepath where the messages dataset is stored
+    categories_filepath - Filepath where the dataset containing categories is stored
+    
+    Output:
+    df - Dataframe containing both messages and categories
+    '''
+    
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     df = messages.merge(categories,on = 'id')
@@ -12,6 +23,16 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
+     '''
+    This function cleans the given dataframe by performing string operations and data type conversions to create dummies for the 'category' values.
+    
+    Input:
+    df - Dataframe containing both messages and categories
+    
+    Output: 
+    df - Dataframe after cleaning
+    '''
+    
     categories = df['categories'].str.split(';',expand = True)
     row = categories.iloc[0]
     category_colnames = [x[:-2] for x in row]
@@ -31,6 +52,17 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
+    '''
+    This function saves the given data in and sql table.
+    
+    Input:
+    df - Dataframe containing both messages and categories
+    database_filename - The name of the data base where the dataframe is to be saved
+    
+    Output: 
+    None
+    '''
+    
     engine = create_engine(f'sqlite:///{database_filename}')
     df.to_sql('disaster', engine, index=False, if_exists='replace')
 
